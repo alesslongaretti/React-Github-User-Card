f7dbdeb24a42f91abea7508ae52e96e9f1cceb98
alesslongaretti <alesslongaretti@gmail.com>
2020-01-21 22:18:54 -0500

    added more styling

--- user-card-project/src/components/FollowersCard.js
+++ user-card-project/src/components/FollowersCard.js
@@ -2,41 +2,37 @@ import React from "react";
 import styled from "styled-components";
 
 const WrapperDiv = styled.div`
-display:flex;
-flex-direction: row;
-align-items: center;
-justify-content: center;
-margin-bottom: 50px;
-`;
-
-const Image = styled.img`
-width: 300px;
-length: 300px;
-border-radius: 10px;
-margin-right: 15px
+  width: 500px;
+  height: 500px;
 `;
 
 const ContentDiv = styled.div`
-width: 800px;
-length: 400px;
-
+  lex-direction: row;
 `;
 
+const Image = styled.img`
+  width: 300px;
+  length: 300px;
+  border-radius: 10px;
+  margin-right: 15px;
+`;
+
+const Span = styled.span`
+  font-weight: 600;
+`;
 
 const FollowersCard = props => {
-    return (
-        <WrapperDiv>
-            <div>
-             <Image src={props.avatar_url}/></div>
-            <ContentDiv>
-             <h3> Login: {props.login}</h3>
-             <p>Github Url: {props.url}</p>
-             </ContentDiv>
-
-            
-        </WrapperDiv>
-    )
-}
-
+  return (
+    <WrapperDiv>
+      <ContentDiv>
+        <Image src={props.avatar_url} />
+        <h3> Login: {props.login}</h3>
+        <p>
+          <Span>Github Url:</Span> {props.url}
+        </p>
+      </ContentDiv>
+    </WrapperDiv>
+  );
+};
 
 export default FollowersCard;
--- user-card-project/src/components/MyCard.js
+++ user-card-project/src/components/MyCard.js
@@ -2,50 +2,51 @@ import React from "react";
 import styled from "styled-components";
 
 const WrapperDiv = styled.div`
-display:flex;
-align-items: center;
-justify-content: center;
-margin-bottom: 50px;
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 50px;
+  background-color: #fff7f8;
 `;
 
 const Image = styled.img`
-width: 300px;
-length: 300px;
-border-radius: 10px;
-margin-right: 15px;
-margin-top: 50px;
-
+  width: 300px;
+  length: 300px;
+  border-radius: 10px;
+  margin-right: 15px;
+  margin-top: 50px;
 `;
 
 const ContentDiv = styled.div`
-width: 800px;
-length: 400px;
-
-
+  width: 800px;
+  length: 400px;
 `;
 
+const Span = styled.span`
+  font-weight: 600;
+`;
 
-
-
-
-
- const MyCard = props => {
-    return (
-        <WrapperDiv> 
-            <div>
-               <Image src={props.avatar_url}/> 
-            </div>
-            <ContentDiv>            
-            <h1>{props.name}</h1>
-            <p>login: {props.login}</p>
-            <p>Followers: {props.followers}</p>
-            <p>Github Url: {props.url}</p>
-            </ContentDiv>
-
-        </WrapperDiv>
-    )
-}
+const MyCard = props => {
+  return (
+    <WrapperDiv>
+      <div>
+        <Image src={props.avatar_url} />
+      </div>
+      <ContentDiv>
+        <h1>{props.name}</h1>
+        <p>
+          <Span>login:</Span> {props.login}
+        </p>
+        <p>
+          <Span>Followers:</Span> {props.followers}
+        </p>
+        <p>
+          <Span>Github Url:</Span> {props.url}
+        </p>
+      </ContentDiv>
+    </WrapperDiv>
+  );
+};
 
 export default MyCard;
 
